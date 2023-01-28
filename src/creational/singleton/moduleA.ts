df9cab780117951d8cfb821277828f27877b7dec
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-01-28 00:57:59 -0300

    teste

--- src/creational/singleton/moduleA.ts
+++
@@ -1,8 +0,0 @@
-import { MyDatabaseFunction } from "./db/myDatabaseFunction"
-
-const myDatabaseClassic = MyDatabaseFunction
-myDatabaseClassic.add({ name: "Luiz", age: 30 })
-myDatabaseClassic.add({ name: "Thiago", age: 22 })
-myDatabaseClassic.add({ name: "Júlio", age: 19 })
-
-export { myDatabaseClassic }
