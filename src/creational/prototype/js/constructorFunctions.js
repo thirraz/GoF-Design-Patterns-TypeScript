1e0368b690cbb05b9f31a106d3755b35af0b548c
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-01-29 16:56:14 -0300

    Practicing Prototype pattern

---
+++ src/creational/prototype/js/constructorFunctions.js
@@ -0,0 +1,33 @@
+function Person(firstName, lastName, age) {
+  this.firstName = firstName
+  this.lastName = lastName
+  this.age = age
+}
+
+const personPrototype = {
+  firstName: "Luiz",
+  lastName: "Miranda",
+  age: 30,
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  },
+}
+
+Person.prototype = Object.create(personPrototype)
+
+function SubPerson(firstName, lastName, age) {
+  Person.call(this, firstName, lastName, age)
+  this.fromSubClass = "OIE"
+}
+
+SubPerson.prototype = Object.create(Person.prototype)
+SubPerson.prototype.constructor = SubPerson
+
+const person1 = new Person("Luiz", "Miranda", 18)
+console.log(person1)
+console.log(person1.fullName())
+
+const person2 = new Person("Helena", "Vieira", 20)
+console.log(person2)
+console.log(person2.fullName())
---
+++ src/creational/prototype/js/object.js
@@ -0,0 +1,17 @@
+const personPrototype = {
+  firstName: "Luiz",
+  lastName: "Miranda",
+  age: 30,
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  },
+}
+
+const anotherPerson = Object.create(personPrototype)
+
+anotherPerson.firstName = "Thiago"
+
+console.log(anotherPerson)
+console.log(anotherPerson.firstName)
+console.log(anotherPerson.fullName())
